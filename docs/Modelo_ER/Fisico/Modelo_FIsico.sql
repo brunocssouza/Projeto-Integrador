20e62573634d94dce719c9e60193c3722db7ed5c
Bruno César <92397493+brunocssouza@users.noreply.github.com>
2025-07-01 21:30:41 -0300

    Update Modelo_FIsico.sql

--- docs/Modelo_ER/Fisico/Modelo_FIsico.sql
+++ docs/Modelo_ER/Fisico/Modelo_FIsico.sql
@@ -1,39 +1,5 @@
-/* Modelo_Logico: */
 
-CREATE TABLE Entrevista (
-	entrevista_id NUMERIC(13) PRIMARY KEY,
-    area VARCHAR(255) NOT NULL,
-    idioma VARCHAR(255) NOT NULL,
-    duracao TIMESTAMP,
-    fk_Aluno_fk_Usuario_usuario_id NUMERIC(13),
-    fk_Aluno_fk_Usuario_cpf VARCHAR(255),
-    fk_Tutor_fk_Usuario_usuario_id NUMERIC(13),
-    fk_Tutor_fk_Usuario_cpf VARCHAR(255),
-    fk_Feedback_feedback_id NUMERIC(13)
-);
-
-CREATE TABLE Feedback (
-	feedback_id NUMERIC(13) PRIMARY KEY,
-    avaliacao_fluencia VARCHAR(255) NOT NULL,
-    sugestao_melhoria VARCHAR(255) NOT NULL,
-    avaliacao_tecnica VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE Aluno (
-    formacao_academica VARCHAR(255) NOT NULL,
-    idioma_nivelamento VARCHAR(255) NOT NULL,
-    fk_Usuario_usuario_id NUMERIC(13),
-    fk_Usuario_cpf VARCHAR(255),
-    PRIMARY KEY (fk_Usuario_usuario_id, fk_Usuario_cpf)
-);
-
-CREATE TABLE Agendamento (
-	agendamento_id NUMERIC(13) PRIMARY KEY,
-    area VARCHAR(255) NOT NULL,
-    idioma VARCHAR(255) NOT NULL,
-    valor MONEY NOT NULL,
-    duracao TIME NOT NULL,
-    "data" DATE NOT NULL,
+    data DATE NOT NULL,
     hora TIME NOT NULL,
     fk_Aluno_fk_Usuario_usuario_id NUMERIC(13),
     fk_Aluno_fk_Usuario_cpf VARCHAR(255),
@@ -152,44 +118,3 @@ ALTER TABLE Endereco_Usuario ADD CONSTRAINT FK_Endereco_Usuario_2
     FOREIGN KEY (fk_Endereco_endereco_id)
     REFERENCES Endereco (endereco_id);
 
-
-
-
-
--- Trigger: Agendamento > Entrevista
-CREATE OR REPLACE FUNCTION inserir_entrevista_apos_agendamento()
-RETURNS TRIGGER AS $$
-DECLARE
-    novo_id_entrevista NUMERIC(13);
-BEGIN
-    SELECT COALESCE(MAX(entrevista_id), 0) + 1 INTO novo_id_entrevista FROM Entrevista;
-
-    INSERT INTO Entrevista (
-        entrevista_id,
-        area,
-        idioma,
-        duracao,
-        fk_Aluno_fk_Usuario_usuario_id,
-        fk_Aluno_fk_Usuario_cpf,
-        fk_Tutor_fk_Usuario_usuario_id,
-        fk_Tutor_fk_Usuario_cpf
-    ) VALUES (
-        novo_id_entrevista,
-        NEW.area,
-        NEW.idioma,
-        NULL,
-        NEW.fk_Aluno_fk_Usuario_usuario_id,
-        NEW.fk_Aluno_fk_Usuario_cpf,
-        NEW.fk_Tutor_fk_Usuario_usuario_id,
-        NEW.fk_Tutor_fk_Usuario_cpf
-    );
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-DROP TRIGGER IF EXISTS trigger_inserir_entrevista ON Agendamento;
-CREATE TRIGGER trigger_inserir_entrevista
-AFTER INSERT ON Agendamento
-FOR EACH ROW
-EXECUTE FUNCTION inserir_entrevista_apos_agendamento();
